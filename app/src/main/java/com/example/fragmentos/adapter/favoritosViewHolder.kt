91e5e641b7c6fd4418393f5c9ae373037689235f
schafickMol <chafmolina17@gmail.com>
2024-10-28 23:42:55 -0600

    terminado

--- app/src/main/java/com/example/fragmentos/adapter/favoritosViewHolder.kt
+++ app/src/main/java/com/example/fragmentos/adapter/favoritosViewHolder.kt
@@ -4,7 +4,6 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.fragmentos.Ensaladas
 import com.example.fragmentos.R
 import com.example.fragmentos.favoritos
 
